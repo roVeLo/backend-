9a48ec1df1b37b6d03f31cc18e55ae3e56cf3e3f
roVeLo <101982518+roVeLo@users.noreply.github.com>
2022-10-31 12:26:24 -0300

    actualizacion backend

---
+++ rvl/src/main/java/com/portafolio/rvl/Repository/RExperiencia.java
@@ -0,0 +1,15 @@
+
+package com.portafolio.rvl.Repository;
+
+import com.portafolio.rvl.Entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+    
+}
